a82c4b35431edd93411d7d6e99bd8456a0ca448f
baraneeswaran <93219520+BARANEESWARAN@users.noreply.github.com>
2024-03-08 23:05:36 +0530

    Update Portfolio.jsx

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -1,100 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import '../Portfolio/portfolio.css';
-import { PortfolioData } from './PortfolioData';
-import { LeftCircleFilled, RightCircleFilled  } from "@ant-design/icons";
-
+import React from 'react'
+import "../Portfolio/portfolio.css"
+import { PortfolioData } from './PortfolioData'
+import img1 from "../../assets/portfolio1.jpg"
+import img2 from "../../assets/portfolio2.jpg"
+import img3 from "../../assets/portfolio3.jpg"
+import img4 from "../../assets/portfolio4.jpg"
+import img5 from "../../assets/portfolio5.png"
+import img6 from "../../assets/portfolio6.jpg"
 function Portfolio() {
-  const [startIndex, setStartIndex] = useState(0);
-  const [itemsPerPage, setItemsPerPage] = useState(3);
  
-  useEffect(() => {
-    const handleResize = () => {
-      const windowWidth = window.innerWidth;
-      if (windowWidth < 730) {
-        // If window width is below 650px, set items per page to 1
-        setItemsPerPage(2);
-      } else {
-        // Otherwise, set items per page to 3
-        setItemsPerPage(3);
-      }
-    };
-
-    // Call handleResize initially to set the initial number of items per page
-    handleResize();
-
-    // Add event listener for window resize
-    window.addEventListener('resize', handleResize);
-
-    // Clean up the event listener when component unmounts
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const handleNext = () => {
-    setStartIndex(prevIndex => prevIndex + 3);
-  };
-
-  const handlePrev = () => {
-    setStartIndex(prevIndex => prevIndex - 3);
-  };
-
-  const totalItems = PortfolioData.length;
-  const itemsPerPages = itemsPerPage;
-  const totalPages = Math.ceil(totalItems / itemsPerPages);
-  const currentPage = Math.ceil((startIndex + 1) / itemsPerPages);
-  const startIndexCurrentPage = (currentPage - 1) * itemsPerPages;
-  const endIndexCurrentPage = Math.min(startIndexCurrentPage + itemsPerPages, totalItems);
-  const itemsForCurrentPage = PortfolioData.slice(startIndexCurrentPage, endIndexCurrentPage);
-
   return (
     <section id='portfolio'>
-      <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
-<div className='projects__container'>
+
+<h5>My Recent Work</h5>
+<h2>Portfolio</h2>
+<div className='container portfolio__container'>
+{
+  PortfolioData.map((Portfolio)=>{
+
+    const {title,id,image,github,demo}=Portfolio
+return(
+
+  <article className='portfolio__item' key={id}>
+<div className='portfolio__item-image'>
+  <img src={image} alt="project"/>
+
+</div>
+<h3>{title}</h3>
+<div className='portfolio__item-btns'>
+<a href={github} className='btn' target='_blank'>Github</a>
+<a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+
+</div>
 
 
-      <div className='portfolio__container'>
-        {itemsForCurrentPage.map(portfolio => (
-          <div className='card__container' key={portfolio.id}>
-            <article className='portfolio__item'>
-              <div className='portfolio__item-image'>
-                <img src={portfolio.image} alt='project' />
-              </div>
-              <h3>{portfolio.title}</h3>
-              <div className='portfolio__item-btns'>
-                <a href={portfolio.github} className='btn' target='_blank'>
-                  Github
-                </a>
-                <a href={portfolio.demo} className='btn btn-primary' target='_blank'>
-                   Demo
-                </a>
-              </div>
-            </article>
-          </div>
-        ))}
-     
+</article>
+)
 
-      </div>
-      <div className='navigation-buttons'>
-        
-         {startIndex > 0 && <button onClick={handlePrev}>
-          
-         <LeftCircleFilled />
-          </button>}
-         
-
-         
-          {startIndex + itemsPerPage < totalItems && <button onClick={handleNext}>
-          <RightCircleFilled /> 
-            </button>}
-          
-       
-        </div>
-      </div>
-     
-    </section>
-  );
+  })
 }
 
-export default Portfolio;
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </section>
+  )
+}
+
+export default Portfolio
