9420289a2659afb8eaf2fb5c1b4657efd4e6aafd
BARANEESWARAN <93219520+BARANEESWARAN@users.noreply.github.com>
2024-07-27 16:13:37 +0530

    Update PortfolioData.jsx

--- src/Component/Portfolio/PortfolioData.jsx
+++ src/Component/Portfolio/PortfolioData.jsx
@@ -1,77 +1,148 @@
 
-export const PortfolioData=[
-  {
-    id:0,
-    image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
-    title:"Social Media Landingpage",
-    github:"https://github.com/BARANEESWARAN/mc_boy-.pluralsight-login",
-    demo:"https://mc-boy-social-media.onrender.com/"
-  },
+// export const PortfolioData=[
+//   {
+//     id:0,
+//     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
+//     title:"Social Media Landingpage",
+//     github:"https://github.com/BARANEESWARAN/mc_boy-.pluralsight-login",
+//     demo:"https://mc-boy-social-media.onrender.com/"
+//   },
 
+//   {
+//     id:1,
+//     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
+//     title:"Skincare Website",
+//     github:"https://github.com/BARANEESWARAN/skincare_wesite",
+//     demo:"https://skincare-products.onrender.com/"
+//   },
+
+//   {
+//     id:2,
+//     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
+//     title:"SmartHome Website",
+//     github:"https://github.com/BARANEESWARAN/smarthome",
+//     demo:"https://mc-boy-smart-home.onrender.com/"
+//   },
+//   {
+//     id:3,
+//     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
+//     title:"Bubble Landingpage",
+//     github:"https://github.com/BARANEESWARAN/bubble_landingpage",
+//     demo:"https://bubble-c5cb.onrender.com/"
+//   },
+//   {
+//     id:4,
+//     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
+//     title:"Pluralsight Login",
+//     github:"https://github.com/BARANEESWARAN/mc_boy-.pluralsight-login",
+//     demo:"https://mc-boy-pluralsight-login.onrender.com"
+//   },
+//   {
+//     id:5,
+//     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
+//     title:"Mytune",
+//     github:"https://github.com/BARANEESWARAN/mytune",
+//     demo:"https://mc-boy-mytune.onrender.com"
+//   },
+//   {
+//     id:6,
+//     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
+//     title:"Gym Website",
+//     github:"https://github.com/BARANEESWARAN/Gym-Website",
+//     demo:"https://mcboy-gym.onrender.com/"
+//   },
+//   {
+//     id:7,
+//     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
+//     title:"Netflix Landingpage",
+//     github:"https://github.com/BARANEESWARAN/netflix",
+//     demo:"https://mcboy-netflix.onrender.com"
+//   },
+//   {
+//     id:8,
+//     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
+//     title:"Microsoft Landingpage",
+//     github:"https://github.com/BARANEESWARAN/microsoft",
+//     demo:"https://mcboy-microsoft.onrender.com"
+//   },
+//   {
+//     id:9,
+//     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
+//     title:"Lokum Documentpage",
+//     github:"https://github.com/BARANEESWARAN/Loruku_Deploy",
+//     demo:"https://clever-lokum-9a7df7.netlify.app"
+//   },
+
+
+    
+  
+//   ]
+
+
+export const PortfolioData=[
   {
     id:1,
     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
-    title:"Skincare Website",
-    github:"https://github.com/BARANEESWARAN/skincare_wesite",
-    demo:"https://skincare-products.onrender.com/"
+    title:"Skincare Wepsite",
+    github:"https://github.com/BARANEESWARAN/skincare_wesite/tree/main",
+    demo:"https://skincare-products.onrender.com"
   },
-
   {
     id:2,
     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
-    title:"SmartHome Website",
-    github:"https://github.com/BARANEESWARAN/smarthome",
-    demo:"https://mc-boy-smart-home.onrender.com/"
+    title:"Social Media",
+    github:"https://github.com/BARANEESWARAN/social-media/tree/main",
+    demo:"https://mc-boy-social-media.onrender.com"
   },
   {
     id:3,
     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
-    title:"Bubble Landingpage",
-    github:"https://github.com/BARANEESWARAN/bubble_landingpage",
-    demo:"https://bubble-c5cb.onrender.com/"
+    title:"Smart Home Wepsite",
+    github:"https://github.com/BARANEESWARAN/smarthome/tree/main",
+    demo:"https://mc-boy-smart-home.onrender.com"
   },
   {
     id:4,
     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
-    title:"Pluralsight Login",
+    title:"Spotify Clone",
     github:"https://github.com/BARANEESWARAN/mc_boy-.pluralsight-login",
-    demo:"https://mc-boy-pluralsight-login.onrender.com"
+    demo:"https://github.com/BARANEESWARAN/spotify-clone/tree/main"
   },
   {
-    id:5,
+    id:2,
     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
     title:"Mytune",
     github:"https://github.com/BARANEESWARAN/mytune",
     demo:"https://mc-boy-mytune.onrender.com"
   },
   {
-    id:6,
+    id:3,
     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
     title:"Gym Website",
     github:"https://github.com/BARANEESWARAN/Gym-Website",
     demo:"https://mcboy-gym.onrender.com/"
   },
   {
-    id:7,
+    id:4,
     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
     title:"Netflix Landingpage",
     github:"https://github.com/BARANEESWARAN/netflix",
     demo:"https://mcboy-netflix.onrender.com"
   },
   {
-    id:8,
+    id:5,
     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
     title:"Microsoft Landingpage",
     github:"https://github.com/BARANEESWARAN/microsoft",
     demo:"https://mcboy-microsoft.onrender.com"
   },
   {
-    id:9,
+    id:6,
     image: "https://cdn.dribbble.com/userupload/8026654/file/original-ed9649c51c598fa619487bbd4c5a2aa0.png?compress=1&resize=1024x768",
     title:"Lokum Documentpage",
     github:"https://github.com/BARANEESWARAN/Loruku_Deploy",
     demo:"https://clever-lokum-9a7df7.netlify.app"
-  },
+  }
 
 
     
